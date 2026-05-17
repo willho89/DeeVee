3a1c1a3f1189d525f1c10a81a3fa99516b943c67
willho89 <willparry79@gmail.com>
2026-05-17 16:26:02 -0400

    Bump alpha version to 0.4.0
    Reflect the current pre-1.0 feature checkpoint in the core and RetroArch metadata.

--- src/libretro.c
+++ src/libretro.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEEVEE_CORE_VERSION "0.1.0-dev"
+#define DEEVEE_CORE_VERSION "0.4.0-alpha"
 
 static retro_environment_t environ_cb;
 static retro_video_refresh_t video_cb;
